363e6eeddab90c0482d86499bc12edae0dc971cd
Pablo Dejuan <pablo@pablo.pm>
2025-09-02 17:49:22 +0100

    Fix RuboCop linting issues
    - Remove trailing whitespace from spec/spec_helper.rb
    - All RuboCop offenses corrected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,19 +18,19 @@
 if ENV['COVERAGE']
   require 'simplecov'
   require 'simplecov_json_formatter'
-  
+
   SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
   SimpleCov.start 'rails' do
     add_filter '/spec/'
     add_filter '/config/'
     add_filter '/vendor/'
-    
+
     add_group 'Models', 'app/models'
     add_group 'Controllers', 'app/controllers'
     add_group 'Helpers', 'app/helpers'
     add_group 'Jobs', 'app/jobs'
     add_group 'Mailers', 'app/mailers'
-    
+
     minimum_coverage 90
   end
 end
